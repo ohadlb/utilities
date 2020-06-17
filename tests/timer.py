bf376552e53cf90be58a090aaacfd27635a6c99b
Ohad Livne <ohad.livne.baron@gmail.com>
2020-06-17 22:10:34 +0300

    Test the timing context

---
+++ tests/timer.py
@@ -0,0 +1,43 @@
+from io import StringIO
+from time import sleep
+import unittest
+from unittest.mock import patch
+
+from ..timer import ExecutionTimer
+
+DELAY = 0.5
+
+
+class Callback:
+    def __call__(self, interval):
+        self.interval = interval
+
+
+class TimerTest(unittest.TestCase):
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_none(self, mock_stdout):
+        with ExecutionTimer(output=None) as e:
+            sleep(DELAY)
+        self.assertGreater(e.interval, DELAY)
+        self.assertEqual(mock_stdout.getvalue(), '')
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_string(self, mock_stdout):
+        with ExecutionTimer() as e:
+            sleep(DELAY)
+        self.assertGreater(e.interval, DELAY)
+        self.assertTrue(mock_stdout.getvalue().startswith('Execution completed in'))
+        self.assertIn(f'{e.interval:0.2f}', mock_stdout.getvalue())
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_callback(self, mock_stdout):
+        cb = Callback()
+        with ExecutionTimer(output=cb) as e:
+            sleep(DELAY)
+        self.assertGreater(e.interval, DELAY)
+        self.assertEqual(e.interval, cb.interval)
+        self.assertEqual(mock_stdout.getvalue(), '')
+
+
+if __name__ == '__main__':
+    unittest.main()
