0275cde1f9e6df5d005d8f1f231bbf64e1b00bb7
Ohad Livne <ohad.livne.baron@gmail.com>
2020-06-21 22:49:01 +0300

    Implement several types of the higher-order map() function

---
+++ functional/map.py
@@ -0,0 +1,43 @@
+from multiprocessing.pool import Pool
+from typing import Callable, Iterable, Optional, Tuple, TypeVar
+
+__all__ = ['dist_map', 'timeout_map', 'tqdm_map_func']
+
+R = TypeVar('R')
+S = TypeVar('S')
+T = TypeVar('T')
+TMap = Callable[[Callable[[S], T], Tuple[Iterable[S], ...]], Iterable[T]]
+
+
+def compose(f: Callable[[S], T], g: Callable[[R], S]) -> Callable[[R], T]:
+    def comp(*args, **kwargs):
+        return f(g(*args, **kwargs))
+
+    return comp
+
+
+def dist_map(nprocs: int, ordered: bool) -> TMap:
+    # NOT creating the pool as a context, to avoid having it terminated upon `return`
+    pool = Pool(processes=nprocs)
+
+    return pool.imap if ordered else pool.imap_unordered
+
+
+def timeout_map(timeout: Optional[float]) -> TMap:
+    import timeout_decorator
+
+    def map_(func: Callable[[S], T], *iterables: Iterable[S]) -> Iterable[T]:
+        for xs in zip(iterables):
+            try:
+                yield timeout_decorator.timeout(timeout)(func)(*xs)
+            except timeout_decorator.TimeoutError:
+                continue
+
+    return map_
+
+
+def tqdm_map_func(map_: TMap, **tqdm_kwargs) -> TMap:
+    from functools import partial
+    import tqdm
+
+    return compose(partial(tqdm.tqdm, **tqdm_kwargs), map_)
